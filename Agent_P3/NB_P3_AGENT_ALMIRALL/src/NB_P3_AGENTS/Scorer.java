30876df490165aa008e3da1a85ec5776fc8a78fb
Marcos Castillo <marcoscastillo@correo.ugr.es>
2021-01-10 20:33:50 +0100

    Ponderación ruta básica

--- Agent_P3/NB_P3_AGENT_ALMIRALL/src/NB_P3_AGENTS/Scorer.java
+++ Agent_P3/NB_P3_AGENT_ALMIRALL/src/NB_P3_AGENTS/Scorer.java
@@ -12,7 +12,7 @@ package NB_P3_AGENTS;
 public class Scorer<T extends Node> {
     //Hay que modificar este método para calcular el coste
     //conforme nuestro criterio
-    public double computeCost(Node from, Node to) {
+    public double computeCostOld(Node from, Node to) {
         double R = 6372.8; // In kilometers
 
         double dX = Math.toRadians(to.getX() - from.getX());
@@ -23,5 +23,20 @@ public class Scorer<T extends Node> {
         double a = Math.pow(Math.sin(dX / 2),2) + Math.pow(Math.sin(dY / 2),2) * Math.cos(x1) * Math.cos(x2);
         double c = 2 * Math.asin(Math.sqrt(a));
         return R * c;
+    }
+    
+    public double computeCost(Node from, Node to) {
+        double distance = 999999999.99;
+        
+        double dX = to.getX() - from.getX();
+        double dY = to.getY() - from.getY();
+        
+        distance = Math.sqrt(Math.pow(dX, 2)+Math.pow(dY, 2)); //Distance between 2 points
+        
+        if(to.getHeight() >= to.MAX_HEIGHT){
+            distance += 999999999.99;
+        }
+        
+        return distance;
     }
 }
